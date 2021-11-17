ebf9cfd80995958140f3c297593889d1db84bc2d
Utsav Italiya <53214436+ujitaliya@users.noreply.github.com>
2021-11-17 16:20:01 -0500

    nothing much updated but had to add comment...

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
-const landingRoutes = require('./landing');
-const projectRoutes = require('./project');
-const ticketRoutes = require('./ticket');
-const path = require('path');
+const landingRoutes = require("./landing");
+const projectRoutes = require("./projects");
+const ticketRoutes = require("./ticket");
+const path = require("path");
 
-const constructorMethod =(app)=>{
-    app.use('/',landingRoutes);
-    app.use('/projectPage',projectRoutes);
-    app.use('/ticketPage',ticketRoutes);
+const constructorMethod = (app) => {
+  app.use("/", landingRoutes);
+  app.use("/projectPage", projectRoutes);
+  app.use("/ticketPage", ticketRoutes);
 
-    app.use('*', (req, res) => {
-        res.sendStatus(404);
-      });
-}; 
+  app.use("*", (req, res) => {
+    res.sendStatus(404);
+  });
+};
 
 module.exports = constructorMethod;
