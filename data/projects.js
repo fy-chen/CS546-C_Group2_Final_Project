2197f839c73eaaff035f38fd19a5a5c9db764ab4
Utsav Italiya <53214436+ujitaliya@users.noreply.github.com>
2021-11-20 13:26:37 -0500

    error handling

--- data/projects.js
+++ data/projects.js
@@ -2,29 +2,64 @@ const mongoCollections = require("../config/mongoCollections");
 const projects = mongoCollections.projects;
 let { ObjectId } = require("mongodb");
 
-async function create(projectName, description) {
-  projectsCollection = await projects();
-
-  let newProject = {
-    projectName: projectName,
-    description: description,
-    user: [],
-    tickets: [],
-  };
-
-  const insertInfo = await projectsCollection.insertOne(newProject);
-  if (insertInfo.insertedCount === 0) {
-    throw new Error(`Could not add a project`);
+let isAppropriateString = (string, name) => {
+  if (!string) {
+    throw new Error(`${name} not provided`);
   }
+  if (typeof string !== "string") {
+    throw new Error(`Provided ${name} is not a string`);
+  }
+  if (string.trim().length === 0) {
+    throw new Error(`Provided ${name} cannot be an empty string`);
+  }
+};
+
+let toObjectId = (id, name) => {
+  isAppropriateString(id, "ProjectId");
+
+  try {
+    parsedId = ObjectId(id);
+  } catch (e) {
+    throw `provided ${name} is not a valid ObjectId , it must be a single String of 12 bytes or a string of 24 hex characters`;
+  }
+
+  return parsedId;
+};
+
+async function create(projectName, description, role) {
+  isAppropriateString(projectName, "Project Name");
+  isAppropriateString(description, "Description");
+  if (isNaN(Number(role))) {
+    throw new Error("Provided role should not be NaN");
+  }
+  if (Number(role) !== 1 || Number(role) !== 2 || Number(role) !== 3) {
+    throw new Error("Provided priority not valid");
+  }
+
+  if (role == 1) {
+    projectsCollection = await projects();
+
+    let newProject = {
+      projectName: projectName,
+      description: description,
+      user: [],
+      tickets: [],
+    };
+
+    const insertInfo = await projectsCollection.insertOne(newProject);
+    if (insertInfo.insertedCount === 0) {
+      throw new Error(`Could not add a project`);
+    }
+  } else throw new Error(`You don't have Admin access`);
 
   return await get(insertInfo.insertedId.toString());
 }
 
 async function get(id) {
-  let id = toObjectId(id);
+  let parsedId = toObjectId(id, "ProjectId");
   projectsCollection = await projects();
 
-  let project = await projectsCollection.findOne({ _id: id });
+  let project = await projectsCollection.findOne({ _id: parsedId });
   if (project === null) {
     throw new Error(`No Project found for id ${id}`);
   }
@@ -46,11 +81,11 @@ async function getAll() {
 }
 
 async function remove(id) {
-  id = ObjectId(id);
+  let parsedId = toObjectId(id, "ProjectId");
   projectsCollection = await projects();
 
-  let project = await get(id.toString());
-  const deletionInfo = await projectsCollection.deleteOne({ _id: id });
+  let project = await get(id);
+  const deletionInfo = await projectsCollection.deleteOne({ _id: parsedId });
 
   if (deletionInfo.deletedCount === 0) {
     throw new Error(`Could not remove Project of Id ${id}`);
@@ -61,6 +96,7 @@ async function remove(id) {
 }
 
 async function rename(id, newProjectName) {
+  isAppropriateString(projectName);
   id = ObjectId(id);
   projectsCollection = await projects();
 
