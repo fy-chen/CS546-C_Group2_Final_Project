91dac83c1f3e10e0cdb3acce6bf377c991519bc1
ChenfengDuan <88074253+ChenfengDuan@users.noreply.github.com>
2021-12-05 16:31:35 -0500

    comment api

---
+++ routes/comment.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data');
+const commentData = data.comments;
+const ticketData = data.tickets;
+
+router.get('/:id', async(req, res) => {
+    try {
+        const comment = await commentData.get(req.params.id);
+    } catch (e) {
+        res.status(500).json({error:e});
+    }
+});
+
+router.get('/getAll', async(req, res) => {
+    try {
+        const comments = await commentData.getAll();
+        res.json(comments);
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+});
+
+router.post('/create', async(req, res) => {
+    const commentData = req.body;
+    let errors = {};
+
+    // try {
+    //     ticketData.isAppropriateString(commentData.text,"text");
+    // } catch (e) {
+    //     errors.text_error = e;
+    // }
+
+    // if(Object.keys(err))
+
+    try {
+        const comment = await commentData.create(commentData.ticketId, commentData.text, commentData.userId);
+        res.json(comment)
+    } catch (e) {
+        res.status(500).json({error : e});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const DeletInfro = await commentData.remove(req.params.id);
+        res.status(200).json(DeletInfro);
+    } catch (e) {
+        res.status(500).json({error:e});
+    }
+});
