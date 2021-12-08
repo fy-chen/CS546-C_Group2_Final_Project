d31208d9e8af59346ca1450e028144019954c335
ChenfengDuan <88074253+ChenfengDuan@users.noreply.github.com>
2021-12-08 18:23:25 -0500

    bar chart can't work(can't get data from backend)

--- bug-tracker-angular/src/app/components/dashboard-home/dashboard-home.component.ts
+++ bug-tracker-angular/src/app/components/dashboard-home/dashboard-home.component.ts
@@ -3,15 +3,23 @@ import { Component, OnInit } from '@angular/core';
 import { ChartType, ChartOptions } from 'chart.js';
 import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
 import { TicketService } from 'src/app/shared/ticket.service';
-
+import { Ticket } from '../tickets';
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
   styleUrls: ['./dashboard-home.component.css']
 })
 export class DashboardHomeComponent implements OnInit {
-  ticket:any
-   // Pie
+  ticket: any
+
+  p1: any
+
+  p2: any
+
+  p3: any
+
+
+  // Pie
   public pieChartOptions: ChartOptions = {
     responsive: true,
   };
@@ -22,28 +30,41 @@ export class DashboardHomeComponent implements OnInit {
   public pieChartPlugins = [];
 
 
-//bar
+  //bar
 
 
 
-public barChartOptions: ChartOptions = {
-  responsive: true,
-};
-  public barChartLabels: Label[] = ["1(less important)","2(medium important)","3(most important)"]
-  public barChartData: SingleDataSet = [150,200,300];
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartLabels: Label[] = ["1(less important)", "2(medium important)", "3(most important)"]
+  public barChartData: SingleDataSet = [100,200,300];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [];
 
-  
+
   constructor(private _ticketService: TicketService) {
     monkeyPatchChartJsTooltip();
     monkeyPatchChartJsLegend();
-   }
+  }
 
   ngOnInit(): void {
-    this._ticketService
+    this._ticketService.getTicketsByPriority()
+      .subscribe((data) => {
+        this.ticket = data;
+
+        this.p1 = this.ticket.ticketsPriority1.length;
+        this.p2 = this.ticket.ticketsPriority2.length;
+        this.p3 = this.ticket.ticketsPriority3.length;
+
+        this.barChartData = [200, 800, 999]
+      });
   }
+
+
+
+
 
 
 }
